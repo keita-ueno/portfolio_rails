1cffad04ccac5af918b0d83a53ba09ddd9f7a0b9
keita-ueno <ueno.7743@gmail.com>
2019-10-21 22:22:05 +0900

    新規登録の追加

--- db/migrate/20191019124805_add_admin_to_users.rb
+++ db/migrate/20191019124805_add_admin_to_users.rb
@@ -1,5 +1,5 @@
 class AddAdminToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_coloumn :users, :admin, :boolean, default: false, null: false
+    add_column :users, :admin, :boolean, default: false, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_220608) do
+ActiveRecord::Schema.define(version: 2019_10_19_124805) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_10_18_220608) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
